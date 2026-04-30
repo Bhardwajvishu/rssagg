2e79976d39927cf2a05386dd2def27114e0c5537
bhardwaj <bhardwaj.vishu@gmail.com>
2026-04-30 09:39:14 +0530

    boilerplate code commited

---
+++ handler_ready.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func handlerReadiness(w http.ResponseWriter,r *http.Request) {
+	respondWithJson(w, http.StatusOK,map[string]string{"status":"ok"})
+}
+
+func handlerErr(w http.ResponseWriter,r *http.Request) {
+	respondWithJson(w, http.StatusInternalServerError,"Internal Server Error")
+}
--- main.go
+++ main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
-	"github.com/joho/godotenv"
+
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
-func main(){
-	fmt.Println("Hello world")
-
+func main() {
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
-	if portString == ""{
+	if portString == "" {
 		log.Fatal("Port is not found")
 	}
 
-	router:= chi.NewRouter()
+	router := chi.NewRouter()
 
-	fmt.Println("Port:", portString)
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz",handlerReadiness)
+	v1Router.Get("/err",handlerErr)
+
+
+	router.Mount("/v1",v1Router)
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on server %v",portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
